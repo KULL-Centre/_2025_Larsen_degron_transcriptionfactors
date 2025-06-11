ddd8ba502492ae8bbf66dd49676e81fb82240df5
Kristoffer Johansson <59598936+enoee@users.noreply.github.com>
2025-06-11 14:28:50 +0200

    Delete run_pap.sh

--- run_pap.sh
+++
@@ -1,4 +0,0 @@
-python PAP.py -v -s 0 ../library/transfac_tiles.fasta > transfac_tiles_pap.txt
-python PAP.py -v -t full-coverage ../library/transfac_proteins.fasta > transfac_proteins_pap.txt
-
-python PAP.py -v -s 0 ../tad_mut.seq > tad_mut_pap.txt
